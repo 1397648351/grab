51b22070f1fe6846b316d5a04f7490845b2823ca
1397648351 <wuzedc@163.com>
2018-05-07 22:39:40 +0800

    1

--- lib/collect.py
+++ lib/collect.py
@@ -1,22 +1,24 @@
 # -*-coding:utf-8-*-
 
 import sys
-from pyocr import pyocr
 from PIL import Image, ImageGrab
+import pytesseract
 
 reload(sys)
 sys.setdefaultencoding("utf-8")
 
-filepath = 'file/3.png'
-img = Image.open(filepath).crop((0, 245, 750, 1115)).convert('L')
-#img.save('file/3.png')
+filepath = 'file/4.jpg'
+img = Image.open(filepath)
+# .crop((0, 245, 750, 1115)).convert('L')
+# img.save('file/3.png')
 
-xy = (255, 525)
-point = img.getpixel(xy)
-print point
 
 width = img.size[0]
 height = img.size[1]
+xy = (width / 2, height / 2)
+point = img.getpixel(xy)
+print point
+print pytesseract.image_to_string(img)
 # for i in xrange(0, width):
 #     for j in xrange(0, height):
 #         print img.getpixel((i, j))
