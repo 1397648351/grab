32f13be3c213d706cee6735d92b451cdaec75492
1397648351 <1397648351@qq.com>
2018-07-05 01:12:11 +0800

    1

--- main.py
+++ main.py
@@ -11,9 +11,15 @@ sys.setdefaultencoding("utf-8")
 if __name__ == "__main__":
     novels = [u'绝世武神']
     engine = pyttsx.init()
+    # voices = engine.getProperty('voices')
+    # for voice in voices:
+    #     print voice
+    # engine.setProperty('voice', 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_ZH-CN_KANGKANG_11.0')  # 发音人
+    engine.setProperty('volume', 1.0)  # 音量
+    engine.setProperty('rate', 200)  # 语速
     for novel in novels:
         engine.say(u'开始抓取 %s' % novel)
-        engine.runAndWait()
-        ss = Novel(novel, 1, 0)
+    engine.runAndWait()
+    # ss = Novel(novel, 1, 0)
     engine.say(u'抓取完成')
     engine.runAndWait()
