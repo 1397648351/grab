671d92cdb9ae834a3bab2932544749ed9639af6b
1397648351 <wuzedc@163.com>
2018-07-11 17:32:24 +0800

    1

---
+++ lib/movie.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+# from collections import deque
+
+# queue = deque(["Eric", "John", "Michael"])
+# queue.append("Terry")
+# queue.append("Graham")
+# while len(queue) > 0:
+#     print queue.popleft()
+
+
+# queue = ["Eric", "John", "Michael"]
+# while len(queue) > 0:
+#     print queue.pop(0)
+
+stack = ["Eric", "John", "Michael"]
+while len(stack) > 0:
+    print stack.pop()
