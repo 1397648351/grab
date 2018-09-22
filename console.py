05323af779c7a8c4b878ab36717773caa0f37519
1397648351 <1397648351@qq.com>
2018-09-22 23:52:31 +0800

    1

--- console.py
+++ console.py
@@ -2,6 +2,7 @@
 # -*-coding: utf-8 -*-
 
 import sys
+import pyttsx
 import os
 import time
 
@@ -10,6 +11,12 @@ sys.setdefaultencoding('UTF-8')
 
 
 if __name__ == "__main__":
+    engine = pyttsx.init()
+    engine.setProperty('volume',1.0)
+    engine.setProperty('rate',200)
+    engine.say('aaaaaaaaaaa')
+    engine.runAndWait()
+    '''
     strs = [
         u'修炼狂潮 \033[32m[42.49%]\033[0m 第九百二十二章 一夜之间，仿佛天塌地陷【六更，求订阅】 已存在！',
         u'修炼狂潮 \033[32m[42.91%]\033[0m 第九百三十章 群凶出牢笼，形势危如累卵 已存在！',
@@ -19,3 +26,4 @@ if __name__ == "__main__":
         print '\r%s' % str,
         sys.stdout.flush()
         time.sleep(1)
+    '''
