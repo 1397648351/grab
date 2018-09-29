521d0f38579ac6d8a4c632bc4f7f01e62a15c105
1397648351 <1397648351@qq.com>
2018-09-29 17:33:50 +0800

    1

--- xiezhen.py
+++ xiezhen.py
@@ -1,11 +1,28 @@
 # !/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import sys, re
+import sys, re, signal,time
+import string
 from bizlayer.xiezhen import Xiezhen
 
 reload(sys)
 sys.setdefaultencoding('UTF-8')
 
+#is_sigint_up = False
+
+
+
 if __name__ == '__main__':
-    Xiezhen.start(1)
+    xz = Xiezhen(1)
+    xz.start()
+    def sigint_handler(signum, frame):
+        is_sigint_up = True
+        print 'catched interrupt signal!'
+        sys.exit(0)
+    signal.signal(signal.SIGINT, sigint_handler)
+    signal.signal(signal.SIGTERM, sigint_handler)
+    is_sigint_up = False
+
+    while not is_sigint_up:
+        print 'aaaa'
+        time.sleep(2)
