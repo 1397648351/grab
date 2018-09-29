f65f2637163113f77192922c95d2495ff28b93e8
1397648351 <1397648351@qq.com>
2018-09-30 01:46:50 +0800

    1

--- xiezhen.py
+++ xiezhen.py
@@ -10,22 +10,21 @@ sys.setdefaultencoding('UTF-8')
 
 kinds = ['xinggan', 'qingchun', 'xiaohua', 'chemo', 'qipao', 'mingxing']
 
+
+def sigint_handler(signum, frame):
+    global xz, is_stopped
+    xz.stop()
+    is_stopped = True
+    while xz.run:
+        pass
+    sys.exit(0)
+
+
 if __name__ == '__main__':
     xz = Xiezhen(1, kind=kinds[1])
     xz.start()
-
-
-    def sigint_handler(signum, frame):
-        global is_stopped
-        xz.stop()
-        is_stopped = True
-        while xz.run:
-            pass
-        sys.exit(0)
-
-
+    is_stopped = False
     signal.signal(signal.SIGINT, sigint_handler)
     signal.signal(signal.SIGTERM, sigint_handler)
-    is_stopped = False
     while not is_stopped and xz.run:
         pass
