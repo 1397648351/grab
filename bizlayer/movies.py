0b8d10a3a75e6d6b307efa12941fe93e55b99362
1397648351 <1397648351@qq.com>
2019-01-06 17:21:08 +0800

    1

--- bizlayer/movies.py
+++ bizlayer/movies.py
@@ -24,8 +24,9 @@ class Movies:
         # self.url = '/html/gndy/dyzz/index.html'
         self.imgpath = 'E:/Project/PHP/bit/public/static/dist/images/movies'
         self.async = 6
+        self.idList = []
         self.movies = []
-        self.sqlList = []
+        # self.sqlList = []
         self.imgList = []
 
     def makedir(self):
@@ -34,6 +35,9 @@ class Movies:
             os.makedirs(path)
 
     def run(self, limit, to=0):
+        ids = DB.fetchall("SELECT id from bs_movie")
+        for id in ids:
+            self.idList.append(str(id[0]))
         print '正在抓取....'
         # self.makedir()
         if to == 0:
@@ -73,11 +77,11 @@ class Movies:
                     threads.pop(i).join()
                     num = num - 1
                     print '剩%s' % num
-        print '正在入库....'
-        if len(self.sqlList) > 0:
-            DB.doTrans(self.sqlList)
-            DB.execute(
-                "DELETE from bs_movie where id in (select a.id from (SELECT min(id) as id FROM bs_movie GROUP BY name HAVING count(name) > 1) a)")
+        # print '正在入库....'
+        # if len(self.sqlList) > 0:
+        #     DB.doTrans(self.sqlList)
+        DB.execute(
+            "DELETE from bs_movie where id in (select a.id from (SELECT min(id) as id FROM bs_movie GROUP BY name HAVING count(name) > 1) a)")
 
     def get_page(self, url):
         html = Grab.get_content(url).replace('xmlns="http://www.w3.org/1999/xhtml" /', '').replace(
@@ -90,6 +94,7 @@ class Movies:
 
     def get_movie(self, url):
         try:
+            sqlList = []
             info = {
                 'id': '',
                 'name': '',
@@ -100,10 +105,12 @@ class Movies:
             }
             pattern = re.compile(r'.*/(?P<id>\d*)\.html')
             info['id'] = re.search(pattern, url).group('id')
+            if info['id'] in self.idList:
+                return
             html = Grab.get_content(url).replace('xmlns="http://www.w3.org/1999/xhtml" /', '').replace(
                 'xmlns="http://www.w3.org/1999/xhtml"', '').decode('gb2312', 'ignore')
             doc = pq(html)
-            info['download'] = doc('#Zoom table a:first-child').text().strip()
+            info['download'] = doc('#Zoom table td:first-child').eq(0).text()
             name = doc('.bd3r .co_area2 .title_all h1').text()
             info['name'] = re.search(ur'.*《(.*)》.*', name).group(1)
             # name = re.search(ur'.*阳光电影www\.ygdy8\.com\.(\W*\d?)\..*', info['download']).group(1).strip()
@@ -115,12 +122,19 @@ class Movies:
             res = re.match(pattern, content)
             if res:
                 info['date'] = res.group('date').strip().replace('年', '-').replace('月', '-').replace('日', '-')
-            pattern = re.compile(ur'.*◎.*评分(?P<score>.*)/10 from .* users.*')
+            pattern = re.compile(ur'.*◎豆瓣评分(?P<score>.*)/10 from .* users.*')
             res = re.match(pattern, content)
             if res:
                 info['score'] = res.group('score').strip()
                 if not info['score']:
                     info['score'] = '0'
+            else:
+                pattern = re.compile(ur'.*◎IMDb评分(?P<score>.*)/10 from .* users.*', re.I)
+                res = re.match(pattern, content)
+                if res:
+                    info['score'] = res.group('score').strip()
+                    if not info['score']:
+                        info['score'] = '0'
             pattern = re.compile(ur'.*◎简介(?P<introduction>.*)◎获奖情况.*<img.*')
             res = re.match(pattern, content)
             if res:
@@ -130,6 +144,8 @@ class Movies:
                 res = re.match(pattern, content)
                 if res:
                     info['introduction'] = res.group('introduction').strip().replace('\'', '\\\'')
+            if len(info['introduction']) >= 1024:
+                info['introduction'] = ''
             # print info['introduction'], url
             # return
             sql = "select * from bs_movie where name='%s' or id='%s'" % (info['name'], info['id'])
@@ -137,7 +153,8 @@ class Movies:
             if res:
                 if int(info['id']) > res[0]:
                     sql = "delete from bs_movie where id='%s'" % res[0]
-                    self.sqlList.append(sql)
+                    sqlList.append(sql)
+                    # self.sqlList.append(sql)
                 else:
                     return
             if info['date']:
@@ -149,12 +166,17 @@ class Movies:
                     info['id'], info['name'], info['score'], info['introduction'], url,
                     info['download'])
             # print sql
-            self.sqlList.append(sql)
+            sqlList.append(sql)
+            if len(sqlList) == 1:
+                DB.execute(sqlList[0])
+            else:
+                DB.doTrans(sqlList)
+            # self.sqlList.append(sql)
         except Exception, ex:
             print str(ex), url
 
 
 if __name__ == "__main__":
     m = Movies()
-    m.run(6, 10)
-    # m.get_movie('https://www.dytt8.net/html/gndy/dyzz/20180913/57447.html')
+    m.run(11, 50)
+    # m.get_movie('https://www.dytt8.net/html/gndy/dyzz/20170414/53727.html')
